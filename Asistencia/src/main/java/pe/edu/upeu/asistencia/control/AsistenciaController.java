827023dd684b91a9e42c4df12caf8b2a93092658
sandro-dtp <kenalemir23@gmail.com>
2025-08-21 12:30:02 -0500

    tracap

--- Asistencia/src/main/java/pe/edu/upeu/asistencia/control/AsistenciaController.java
+++ Asistencia/src/main/java/pe/edu/upeu/asistencia/control/AsistenciaController.java
@@ -1,29 +1,21 @@
 package pe.edu.upeu.asistencia.control;
 
 import javafx.fxml.FXML;
+
 import javafx.scene.control.Label;
-import javafx.scene.control.TabPane;
 import javafx.scene.control.TextField;
 import org.springframework.stereotype.Controller;
-
-import javax.swing.text.TabExpander;
-
 @Controller
 public class AsistenciaController {
-
-
-    @FXML TextField TxtNum1,TxtNum2;
+    @FXML TextField txtNum1, txtNum2;
     @FXML Label txtResult;
 
     @FXML
-
-    public void sumar (){
-        double num1 = Double.parseDouble(TxtNum1.getText());
-        double num2 = Double.parseDouble(TxtNum2.getText());
-        double resultado = num1 + num2;
-        txtResult.setText(Double.toString(resultado));
-
-
+    public void sumar(){
+        double num1 = Double.parseDouble(txtNum1.getText());
+        double num2 = Double.parseDouble(txtNum2.getText());
+        double resultado= num1+num2;
+        txtResult.setText(String.valueOf(resultado));
     }
 
 }
