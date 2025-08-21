5cd83f0b951b4156fb0cbfe5054c1ca33bda4e21
sandro-dtp <kenalemir23@gmail.com>
2025-08-21 11:41:55 -0500

    trabajo2

---
+++ Asistencia/src/main/java/pe/edu/upeu/asistencia/Aplication.java
@@ -0,0 +1,9 @@
+package pe.edu.upeu.asistencia;
+
+public class Aplication {
+    public static void main(String[] args) {
+        System.out.println("iniciando asistencia");
+        AsistenciaApplication.main(args);
+
+    }
+}
